55104d3d6748fbd93fcb8594a31ec2f5e696877d
hooniegit <130134750+hooniegit@users.noreply.github.com>
2023-12-10 21:34:04 +0900

    231210 test: line noti 전송 기능 추가 및 테스트

--- demo/fastapi/libs/log_libs.py
+++ demo/fastapi/libs/log_libs.py
@@ -1,28 +1,18 @@
+import os, sys
 
-def get_git_branch():
-    import os, subprocess
-    
-    now_dir = os.path.dirname(os.path.abspath(__file__))
-    
-    try:
-        # git -C [path] rev-parse --abbrev-ref HEAD
-        command = ["git", "-C", now_dir, "rev-parse", "--abbrev-ref", "HEAD"]
-        result = subprocess.run(command, capture_output=True, text=True, check=True)
-        return result.stdout.strip()
-    
-    except subprocess.CalledProcessError as e:
-        print(f"Error Appeared: {e}")
-        return None
+now_dir = os.path.dirname(os.path.abspath(__file__))
+lib_dir = os.path.join(now_dir, "../libs")
+sys.path.append(lib_dir)
+
+from os_libs import *
 
 
 def file_handler(log_dir:str):
     import logging
     from datetime import datetime
     
-    # set log format
     log_format = "%(asctime)s - %(levelname)s - %(message)s"
 
-    # set file handler
     file_handler = logging.FileHandler(
         f"{log_dir}/{datetime.now().strftime('%Y-%m-%d')}.log", 
         encoding="utf-8")
@@ -34,27 +24,34 @@ def file_handler(log_dir:str):
 def setup_logger(name:str, level=None, log_dir=None):
     import logging
 
-    # create logger
     logger = logging.getLogger(name)
     
-    # set logger level
     if level == None:
         logger.setLevel(logging.DEBUG)
-        # check github branch & change log level
-        git_branch = get_git_branch()
+        git_branch = get_git_branch() # module
         if git_branch and ('rel' in git_branch or 'main' in git_branch):
             logger.setLevel(logging.INFO)
     else:
         try:
             logger.setLevel(level)
         except:
-            # print error message
             print("CANNOT SET LOG LEVEL - WRONG 'level' PARAMS")
             return
 
-    # add file handler
     if log_dir != None:
         handler = file_handler(log_dir)
         logger.addHandler(handler)
 
     return logger
+
+
+def remove_logger(logger):
+    try:
+        for handler in logger.handlers[:]:
+            logger.removeHandler(handler)
+        for filter_ in logger.filters[:]:
+            logger.removeFilter(filter_)
+    except Exception as E:
+        error_message = f"ERROR APPEARED - {E}"
+        logger.error(error_message)
+        print(error_message)
