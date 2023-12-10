827695e64d5e78914633aa616361888a4627e1df
hooniegit <130134750+hooniegit@users.noreply.github.com>
2023-12-10 21:34:04 +0900

    231210 test: line noti 전송 기능 추가 및 테스트

--- demo/fastapi/middlewares/log_logger.py
+++ demo/fastapi/middlewares/log_logger.py
@@ -6,7 +6,8 @@ log_dir = os.path.join(now_dir, "../logs")
 
 sys.path.append(lib_dir)
 
-from define_logs import *
+from log_libs import *
+from os_libs import *
 
 from fastapi import HTTPException
 from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
@@ -21,33 +22,22 @@ class DemoMiddleware(BaseHTTPMiddleware):
         logger_name = endpoint_path.replace("/", "_")[1:]
 
         log_file_dir = f"{log_dir}/{logger_name}"
-        if not os.path.exists(log_file_dir):
-            os.makedirs(log_file_dir)
-        else:
-            pass
+        check_mkdirs(log_file_dir) # module
 
-        # can set - name, level, log_dir
-        logger = setup_logger(name=logger_name, log_dir=log_file_dir)
-
-        print(">>>>>>>>>>>>> START WORKING")
-        logger.info(">>>>>>>>>>>>> START WORKING") 
+        logger = setup_logger(name=logger_name, log_dir=log_file_dir) # module - can set: name, level, log_dir
         
         print(f"logger name: {logger_name}")
         logger.info(f"logger name: {logger_name}") 
            
         try:
             response = await call_next(request)
-            for handler in logger.handlers[:]:
-                logger.removeHandler(handler) # remove handlers
-            for filter_ in logger.filters[:]:
-                logger.removeFilter(filter_) # remove filters
+            remove_logger(logger) # module
             return response
         
         except Exception as e:
-            logger.error(f"Internal Server Error: {str(e)}")
-            for handler in logger.handlers[:]:
-                logger.removeHandler(handler) # remove handlers
-            for filter_ in logger.filters[:]:
-                logger.removeFilter(filter_) # remove filters
+            message = f"Internal Server Error - {endpoint_path} - {str(e)}"
+            logger.error(message)
+            send_line_notification_thread(message=message) # module
+            remove_logger(logger) # module
             raise HTTPException(status_code=500, detail="Internal Server Error")
         
