84f0b0bcbdf8a3a37d4150cba08cf14661176f62
hooniegit <neivekim76@gmail.com>
2023-12-19 22:43:19 +0900

    231219 test: 비동기 테스트

--- demo/fastapi/utils/demo.py
+++ demo/fastapi/utils/demo.py
@@ -2,4 +2,4 @@
 def create_error(received_data):
     DATA = int(received_data)
     print(DATA)
-        
+    
--- demo/kafka/basic/producer.py
+++ demo/kafka/basic/producer.py
@@ -1,14 +1,17 @@
+import asyncio
 
 def delivery_report(err, msg):
     if err is not None:
         print('Message Deliver Failed - {}'.format(err))
     else:
         print('Message Delivered - {} [{}]'.format(msg.topic(), msg.partition()))
-
-
+        
+        
 def publish_message(broker:str, topic:str, key:str, message:str, partition:int=0):
     from confluent_kafka import Producer
+    from time import time
     
+    start_time = time()
     conf = {'bootstrap.servers': broker}
     producer = Producer(conf)
     producer.produce(topic, 
@@ -17,11 +20,12 @@ def publish_message(broker:str, topic:str, key:str, message:str, partition:int=0
                      value=message, 
                      callback=delivery_report)
     producer.flush()
+    for i in range(1, 1000):
+        print("HELLO, KAFKA!")
+    end_time = time()
+    print(end_time-start_time)
+    
 
-
-if __name__ == "__main__":
-    publish_message(broker="localhost:9092",
-                    topic="sample_topic",
-                    key="1",
-                    partition=0,
-                    message="Hello, World!")
+if __name__ == "__main__": 
+    pass
+    
